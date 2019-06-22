69fe38ce7cbba72d23aa7ea1d584d22fa05fcdc2
pedro <osodrac_ordep@hotmail.com>
2019-06-22 08:52:28 -0500

    Creates models Category and Inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(
+        max_length=95,
+        verbose_name="Categoria")
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        verbose_name="Categoria del producto",
+        related_name="productos",
+        on_delete=models.CASCADE)
+
+    name = models.CharField(
+        max_length=95,
+        verbose_name="Nombre del producto")
+
+    image = models.CharField(
+        max_length=500,
+        verbose_name="Imagen de producto")
