0ac3cb5fb08bfd627e235beb3ba9481c40b274b5
pedro <osodrac_ordep@hotmail.com>
2019-07-02 18:38:36 -0500

    Cambia el motor de BD a Mysql.

---
+++ data.sql
@@ -0,0 +1,22 @@
+INSERT INTO auth_user VALUES(
+    1,'pbkdf2_sha256$150000$SWtUf35Bkdz7$0mS/sDx3EVf2cZXeAT6syQQmEWZ4SrHbzo9f6BBU/oo=','2019-07-02 23:04:15.237693',1,'admin',
+    '','','admin@admin.com',1,1,'2019-06-22 13:55:49.783347');
+
+INSERT INTO inventory_category VALUES(1,'Laptops');
+INSERT INTO inventory_category VALUES(2,'Books');
+INSERT INTO inventory_category VALUES(3,'Phones');
+INSERT INTO inventory_category VALUES(4,'Movies');
+
+INSERT INTO inventory_product VALUES(1,'Alienware','https://i.dell.com/sites/csimages/Merchandizing_Imagery/all/alienware-m15-570x428.png',1);
+INSERT INTO inventory_product VALUES(2,'Mac','https://http2.mlstatic.com/laptop-apple-macbook-air-core-i5-11-pulgadas-nueva-D_NQ_NP_610907-MLM26625337270_012018-Q.jpg',1);
+INSERT INTO inventory_product VALUES(3,'Lenovo Yoga','https://http2.mlstatic.com/laptop-lenovo-yoga-510-14-amd-a9-9410-ram-8gb-disco-1tb-w10h-D_NQ_NP_749738-MLM30992816578_062019-Q.jpg',1);
+INSERT INTO inventory_product VALUES(4,'Homo Deus','https://cdn.gandhi.com.mx/media/catalog/product/i/m/image_1165_1_296746.jpg',2);
+INSERT INTO inventory_product VALUES(5,'Homo Sapiens','https://cdn-images-1.medium.com/max/1200/1*EW6oQNjTw0cXQtKtO_CtOg.jpeg',2);
+INSERT INTO inventory_product VALUES(6,'Becoming Michelle','https://hive.dmmserver.com/media/356/97802419/9780241982983.jpg',2);
+INSERT INTO inventory_product VALUES(7,'Samsumg S10','https://images-na.ssl-images-amazon.com/images/I/61YVqHdFRxL._SX569_.jpg',3);
+INSERT INTO inventory_product VALUES(8,'Huawei P30','https://i.gadgets360cdn.com/products/large/1553612597_635_huawei_p30_pro.jpg',3);
+INSERT INTO inventory_product VALUES(9,'Iphone X','https://http2.mlstatic.com/celular-libre-iphone-x-58-256gb-12mp7mp-3g-D_NQ_NP_761839-MCO26661643827_012018-Q.jpg',3);
+INSERT INTO inventory_product VALUES(10,'Amigos','https://images-na.ssl-images-amazon.com/images/I/81U%2Brm4qtwL._SY445_.jpg',4);
+INSERT INTO inventory_product VALUES(11,'Princesa mononoke','https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/princess-mononoke-poster.jpg?itok=PJ4eYAdx',4);
+INSERT INTO inventory_product VALUES(12,'Ex Machina','https://images-na.ssl-images-amazon.com/images/I/91JXlGAE6gL._SY445_.jpg',4);
+
--- inventory_web_api/settings.py
+++ inventory_web_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'inventory_web_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_inventory',
+        'USER': 'pedro',
+        'PASSWORD': 'Mysql.123',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
